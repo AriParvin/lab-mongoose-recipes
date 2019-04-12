7eb12f5eeca15e23a6ce90e8442b0ee9387b02f0
Arian Parvin <arian.parvin@parvin.de>
2019-04-12 02:29:02 +0200

    Iteration 4

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose")
+const Recipe = require("./models/Recipe") // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js") // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+mongoose
+	.connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
+	.then(() => {
+		console.log("Connected to Mongo!")
+	})
+	.catch(err => {
+		console.error("Error connecting to mongo", err)
+	})
+mongoose.connection.on("connected", () => {
+	console.log("connected")
+})
+mongoose.connection.on("error", () => {
+	console.error("error")
+})
+mongoose.connection.on("disconnected", () => {
+	console.log("disconnected")
+})
+process.on("SIGINT", () => {
+	mongoose.connection.close(() => {
+		console.log("Mongoose default connection disconnected through app termination")
+		process.exit(0)
+	})
+})
+console.log(data)
+Recipe.insertMany(data, err => {
+	if (err) {
+		console.log("Something went wrong:", err)
+	} else {
+		console.log("multiple recipes saved")
+	}
+})
+Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+	.then(console.log("changed duration to: 100"))
+	.catch(console.log("Something went wrong.."))
 
+Recipe.deleteOne({ title: "Carrot Cake" })
+	.then(console.log("removed CC"))
+	.catch(console.log("Something went wrong.."))
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,9 +1,42 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
 const recipeSchema = new Schema({
-  // TODO: write the schema
-});
+	// TODO: write the schema
+	title: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	level: {
+		type: String,
+		enumValues: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+		ingredients: Array,
+		cuisine: {
+			type: String,
+			required: true
+		},
+		dishType: {
+			type: String,
+			enumValues: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]
+		},
+		image: {
+			type: String,
+			default: "https://images.media-allrecipes.com/images/75131.jpg "
+		},
+		duration: {
+			type: Number,
+			required: true,
+			min: 0
+		},
+		creator: {
+			type: String
+		},
+		created: {
+			type: Date,
+			default: "today"
+		}
+	}
+})
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
-module.exports = Recipe;
+const Recipe = mongoose.model("Recipe", recipeSchema)
+module.exports = Recipe
